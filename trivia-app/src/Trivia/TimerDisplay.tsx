7398ef5f3f3944b9efd86272dcc9a4f752c25aa3
JamesCarney01 <93592851+JamesCarney01@users.noreply.github.com>
2023-06-13 00:24:24 +1000

    Delete trivia-app directory

--- trivia-app/src/Trivia/TimerDisplay.tsx
+++
@@ -1,9 +0,0 @@
-interface TimerDisplayProps {
-    timer: number;
-  }
-  
-  const TimerDisplay: React.FC<TimerDisplayProps> = ({ timer }) => {
-    return <div  className="text-white text-2xl font-bold mt-4 text-center">Your time: <span id="timer-display">{timer}</span></div>;
-  };
-
-  export default TimerDisplay
